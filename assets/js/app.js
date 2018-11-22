5caf8255e952284134f5ae62893820a9a6bdaca4
henriksimen <henriksimen@gmail.com>
2018-11-22 12:36:58 -0500

    jekyll-5

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,22 +1,36 @@
-//Color variables
-const primaryColor = '#3A2E62';
-const secondaryColor = '#EE7F2F';
+// //Color variables
+// const primaryColor = '#3A2E62';
+// const secondaryColor = '#EE7F2F';
 
-//Body BG Color
-document.querySelector('body').style.backgroundColor = primaryColor;
+// //Initializer
 
-//The Rest
-const theDOM = document.querySelectorAll('article h2, article p, article a, footer p, footer ul, footer a, hr, .attribution'); // returns array of all elements in the DOM
-//Loop through all elements
-Array.from(theDOM).forEach(function(element){
-  if(element.tagName === 'HR'){ // check if element being looped through is HR (caps are important) element
-    element.style.backgroundColor = secondaryColor;
-  } else if (element.className === 'attribution') {
-    element.style.borderColor = secondaryColor;
-  } else if (element.tagName === 'A'){
-    element.style.borderColor = secondaryColor;
-  }
-  element.style.color = secondaryColor;
-})
+// bgColor();
+
+// //Functions
+
+// function bgColor(){
+//   const bg = document.querySelector('body');
+//   if(bg.id === 'archive-page'){
+//     bg.classList.add('archive')
+//   } else {
+//     bg.style.backgroundColor = primaryColor;
+//   }
+// }
+
+
+
+// //The Rest
+// const theDOM = document.querySelectorAll('article h2, article a, footer p,  footer ul,  footer a, hr, .attribution'); // returns array of all elements in the DOM
+// //Loop through all elements
+// Array.from(theDOM).forEach(function(element){
+//   if(element.tagName === 'HR'){ // check if element being looped through is HR (caps are important) element
+//     element.style.backgroundColor = secondaryColor;
+//   } else if (element.className === 'attribution') {
+//     element.style.borderColor = secondaryColor;
+//   } else if (element.tagName === 'A'){
+//     element.style.borderColor = secondaryColor;
+//   }
+//   element.style.color = secondaryColor;
+// })
 
 
